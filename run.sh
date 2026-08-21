a7cce98ed18c8140c0b378470e2d091cb0e6af7d
Kemsekov <pikova98@gmail.com>
2026-08-21 17:22:43 +0500

    Use correct index url

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 CUDA_VER=$(nvidia-smi | grep -oP 'CUDA Version: \K[0-9]+\.[0-9]+' | tr -d '.')
 echo "CUDA VERSION", $CUDA_VER
 
-pip install -r requirements.txt --extra-index-url https://pytorch.org{$CUDA_VER}
+pip install -r requirements.txt --index-url https://download.pytorch.org/whl/cu$CUDA_VER
+
 if [ ! -f "/root/.cache/pip/diffusers.tar.gz" ]; then
     wget -O /root/.cache/pip/diffusers.tar.gz https://github.com/huggingface/diffusers/archive/a00d536450c6cb83824366f4b4d22426cba9165c.tar.gz
 fi
